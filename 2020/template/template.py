aee4529d279b91b59308939b49c20030347b03fc
Luna Halm <thereal1024@gmail.com>
2020-12-15 15:30:22 -0800

    Add template and empty Dec 01 solution

---
+++ 2020/template/template.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def solution(lines):
+    pass
+
+if __name__ == '__main__':
+    file_in = open('input.txt')
+    lines = (line.trim() for line in file_in.readlines())
+    solution(lines)
