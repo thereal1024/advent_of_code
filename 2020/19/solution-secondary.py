615e0026bf6467711828eeada8a0f0d0edafba18
Luna Halm <thereal1024@gmail.com>
2020-12-19 01:15:46 -0800

    fix Dec 19 part 2, don't actually need recursive rule unroll hack
    Somehow I had fixed the previous infinite recursion I previous had. It must have been a bug.

--- 2020/19/solution-secondary.py
+++ 2020/19/solution-secondary.py
@@ -15,11 +15,9 @@ def proc_rule(rule):
     return rid, optidl
 
 def valid_counts(rules, msg, rule_n, offset):
-    if offset >= len(msg):
-        return set()
     optl = rules[rule_n]
     if type(optl) == str:
-        return set([1]) if msg[offset] == optl else set()
+        return set([1]) if offset < len(msg) and msg[offset] == optl else set()
     accept_lens = set()
     for opt in optl:
         test_offsets = set([offset])
@@ -41,13 +39,8 @@ def solution(sections):
     msgs = [msg.strip() for msg in msgs.split('\n') if msg]
     
     rules = dict(proc_rule(rule) for rule in rules)
-    # rule must match at least 1 character
-    # e.g. expansion_max =  max(len(msg) for msg in msgs)
-    # but we can make an assumption that it may be lower
-    expansion_max = 8
-    # fully accurate substitition "approximations"
-    rules[8] = [[42] * i for i in range(1, expansion_max+1)]
-    rules[11] = [[42] * i + [31] * i for i in range(1, math.ceil(expansion_max / 2)+1)]
+    rules[8] = [[42], [42, 8]]
+    rules[11] = [[42, 31], [42, 11, 31]]
     valid_msgs = sum(is_valid(rules, msg) for msg in msgs)
     print(f"Solution: {valid_msgs}")
 
