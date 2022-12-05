618cb96a215e73258cac241caa6ef7a25fbb36e1
Luna Halm <thereal1024@gmail.com>
2022-12-04 22:49:36 -0800

    add 2022/05 solutions

---
+++ 2022/05/solution.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from collections import deque
+
+def parse_move(movestr):
+    tokens = movestr.split(' ')
+    return (int(tokens[1]), int(tokens[3])-1, int(tokens[5])-1)
+
+def solution(sections):
+    crates, moves = sections
+    crates = crates.split('\n')[:-1]
+    cols = (len(crates[0])+1) // 4
+    height = len(crates)
+    
+    cratescols = [deque() for i in range(cols)]
+    for col in range(cols):
+        for ht in range(height):
+            val = crates[ht][col*4+1]
+            if val != ' ':
+                cratescols[col].appendleft(val)
+
+    moves = moves.split('\n')[:-1]
+    moves = (parse_move(move) for move in moves)
+    
+    for count, src, dst in moves:
+        for _ in range(count):
+            e = cratescols[src].pop()
+            cratescols[dst].append(e)
+    
+    solution = ''.join(cratecol[-1] for cratecol in cratescols)
+    print(f'Solution: {solution}')
+    
+
+if __name__ == '__main__':
+    file_in = open('input.txt')
+    sections = file_in.read().split('\n\n')
+    solution(sections)
