e807279174a515c2daa8f0025559c2ed0cf337e1
Luna Halm <thereal1024@gmail.com>
2022-12-19 01:38:06 -0800

    add 2022/19 solutions

---
+++ 2022/19/solution-secondary.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from collections import namedtuple
+import functools
+import math
+from tqdm import tqdm
+
+TIME = 32
+
+Blueprint = namedtuple('Blueprint', ['id', 'costs'])
+
+def parse_blueprint(line):
+    tok = line.split(' ')
+    ore = (int(tok[6]), 0, 0, 0)
+    clay = (int(tok[12]), 0, 0, 0)
+    obs = (int(tok[18]), int(tok[21]), 0, 0)
+    geo = (int(tok[27]), 0, int(tok[30]), 0, 0)
+    return Blueprint(
+        id=int(tok[1][:-1]),
+        costs=[ore, clay, obs, geo]
+    )
+
+def most_geo(bp, time):
+    max_items = tuple(max(mat) for mat in zip(*bp.costs, (0, 0, 0, float('inf'))))
+    
+    search_best = 0
+    
+    @functools.cache
+    def geo_rec(bots, mats, time):
+        nonlocal bp, max_items, search_best
+        
+        geos = mats[3]
+        if time == 0:
+            search_best = max(search_best, geos)
+            return geos
+        
+        geobots = bots[3]
+        # if it is already not possible to beat global search, bail this path
+        if geos + (time * (2 * geobots + time - 1) // 2) <= search_best:
+            return -1
+        
+        after_build = [(bots, mats)]
+        
+        for bot_i, recipe in enumerate(bp.costs):
+            # do not need more bots of type than maximum material type needed in a tick
+            if bots[bot_i] >= max_items[bot_i]:
+                continue
+            if all(matamt >= reccost for matamt, reccost in zip(mats, recipe)):
+                newbots = tuple(botc + (i == bot_i) for i, botc in enumerate(bots))
+                newmats = tuple(matamt - reccost for matamt, reccost in zip(mats, recipe))
+                after_build.append((newbots, newmats))
+        
+        best = 0
+        for newbots, newmats in after_build:
+            newmats = tuple(min(matamt + botc, time * maxi) for matamt, botc, maxi in zip(newmats, bots, max_items))
+            best = max(best, geo_rec(newbots, newmats, time - 1))
+        
+        return best
+    
+    return geo_rec((1, 0, 0, 0), (0, 0, 0, 0), time)
+
+def solution(lines):
+    blueprints = [parse_blueprint(line) for line in lines][:3]
+    geos = [most_geo(bp, TIME) for bp in tqdm(blueprints)]
+    soln = math.prod(geos)
+    print(f'Solution: {soln}')
+
+if __name__ == '__main__':
+    file_in = open('input.txt')
+    lines = (line.strip() for line in file_in.readlines())
+    solution(lines)
