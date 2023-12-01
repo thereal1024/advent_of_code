b0b3f4fbf1d712fbc2eac0f05234ce64194aa209
Luna Halm <thereal1024@gmail.com>
2023-11-30 20:59:17 -0800

    add 2023 template

---
+++ 2023/create_project.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from shutil import copy2
+
+if __name__ == '__main__':
+    date = sys.argv[1]
+    directory = '{:02d}'.format(int(date))
+    os.mkdir(directory)
+    copy2('template/input.txt', directory + '/input.txt')
+    copy2('template/template.py', directory + '/solution.py')
+    copy2('template/template.py', directory + '/solution-secondary.py')
