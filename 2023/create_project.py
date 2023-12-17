a156435d268d52727d4aeb2a3bb984d333f544a4
Luna Halm <thereal1024@gmail.com>
2023-12-16 22:25:46 -0800

    fix create project scripts for fresh checkouts

--- 2023/create_project.py
+++ 2023/create_project.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     date = sys.argv[1]
     directory = '{:02d}'.format(int(date))
     os.mkdir(directory)
-    copy2('template/input.txt', directory + '/input.txt')
+    copy2('template/blank.txt', directory + '/input.txt')
     copy2('template/template.py', directory + '/solution.py')
     copy2('template/template.py', directory + '/solution-secondary.py')
